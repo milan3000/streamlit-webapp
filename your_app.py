5e8d661843d40bd8d6b206734f57467bc1a6cdeb
milan3000 <milanw12@gmail.com>
2024-01-22 22:48:28 +0100

    changed spacing

--- your_app.py
+++ your_app.py
@@ -41,7 +41,7 @@ with st.container():
     left_column, middle_column, right_column = st.columns((3,1,3))
     with left_column:
         st.subheader("Hello, welcome to the", anchor=False)
-        left_mini_column, right_mini_column = st.columns([2,8])
+        left_mini_column, right_mini_column = st.columns([1,9])
         with left_mini_column:
             st.image("favicon_noback_green.svg", width=70)
         with right_mini_column:
