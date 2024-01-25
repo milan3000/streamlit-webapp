c9584052974f69c779e1b1ae2bc83ce4a65294ee
milan3000 <milanw12@gmail.com>
2024-01-25 15:42:49 +0100

    changed threshholds and color of traffic light

--- traffic_light.py
+++ traffic_light.py
@@ -19,7 +19,7 @@ def generate_traffic_light_html(state):
                 height: 320px;
                 border-radius: 20px;
                 overflow: hidden;
-                background-color: grey;
+                background-color: darkgrey;
             }}
 
             .circle {{
