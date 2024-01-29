b53ad287d68cd0f798b37b9dc6ab950320e51396
milan3000 <milanw12@gmail.com>
2024-01-29 12:25:58 +0100

    removed the non-commercial repuirement due to conflict with data-provider license

--- pages/terms.py
+++ pages/terms.py
@@ -33,7 +33,7 @@ with st.container():
                 and indicate if changes were made . You may do so in any reasonable manner, 
                 but not in any way that suggests the licensor endorses you or your use.
                 """)
-        st.write("**NonCommercial** — You may not use the material for commercial purposes .")
+#        st.write("**NonCommercial** — You may not use the material for commercial purposes .")
         st.write("""
                 **No additional restrictions** — You may not apply legal terms or technological measures 
                 that legally restrict others from doing anything the license permits.
